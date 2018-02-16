424e80fbacf3cd733704912e1ca1ff49a4755dbc
Jess Moran <j-es-itza@hotmail.com>
2018-02-16 09:02:36 -0600

    V1 complete

---
+++ js/data.js
@@ -0,0 +1,52 @@
+var commentsData = [
+  {
+  content: "hi",
+  style :{
+    color :"yellow",
+    background :"black"
+  }
+},
+{
+  content: "bye",
+  style :{
+    color :"white",
+    background :"purple"
+  }
+},
+
+];
+
+var commentsContainer = document.getElementById("comments-container");
+
+function drawComments (){
+  commentsContainer.innerHTML = " ";
+  for (var i = 0; i < commentsData.length; i++){
+    var sectionDOM = createDomCommentFromData(commentsData [i]);
+    commentsContainer.appendChild(sectionDOM);
+  }
+}
+
+function createDomCommentFromData (data){
+  var commentContent = data.content;
+  var commentDom = document.createElement("section");
+  commentDom.innerHTML = commentContent;
+
+  Object.assign(commentDom.style,data.style)
+  return commentDom;
+    }
+
+ drawComments ();
+
+var sendBtn = document.getElementById("send-btn");
+
+sendBtn.addEventListener("click", addComment);
+
+function addComment (){
+  var commentPreview = document.getElementById("comments-container");
+  var newCommentText = commentPreview.innerHTML;
+  var newComment = { content :null, style :{} };
+  newComment.content = newCommentText;
+  Object.assign(newComment.style,commentPreview.style);
+  commentsData.unshift(newComment);
+  drawComments ();
+}
